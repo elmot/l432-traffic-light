a325fce907cdc5ddc5feecd3c1d12bc72ed98275
Ilia Motornyi <ilia.motornyi@jetbrains.com>
2026-04-20 16:45:13 +0300

    Add UART diagnostics

--- Core/Src/traffic_light.c
+++ Core/Src/traffic_light.c
@@ -1,4 +1,7 @@
 #include "traffic_light.h"
+#include <stdio.h>
+
+extern UART_HandleTypeDef huart2;
 
 void traffic_light_loop()
 {
@@ -9,6 +12,7 @@ void traffic_light_loop()
         switch (current_phase)
         {
             case PHASE_RED:
+                printf("PHASE: RED\r\n");
                 HAL_GPIO_WritePin(SEM_RED_GPIO_Port, SEM_RED_Pin, GPIO_PIN_SET);
                 HAL_GPIO_WritePin(SEM_YELLOW_GPIO_Port, SEM_YELLOW_Pin, GPIO_PIN_RESET);
                 HAL_GPIO_WritePin(SEM_GREEN_GPIO_Port, SEM_GREEN_Pin, GPIO_PIN_RESET);
@@ -17,6 +21,7 @@ void traffic_light_loop()
                 break;
 
             case PHASE_RED_YELLOW:
+                printf("PHASE: RED_YELLOW\r\n");
                 HAL_GPIO_WritePin(SEM_RED_GPIO_Port, SEM_RED_Pin, GPIO_PIN_SET);
                 HAL_GPIO_WritePin(SEM_YELLOW_GPIO_Port, SEM_YELLOW_Pin, GPIO_PIN_SET);
                 HAL_GPIO_WritePin(SEM_GREEN_GPIO_Port, SEM_GREEN_Pin, GPIO_PIN_RESET);
@@ -25,6 +30,7 @@ void traffic_light_loop()
                 break;
 
             case PHASE_GREEN:
+                printf("PHASE: GREEN\r\n");
                 HAL_GPIO_WritePin(SEM_RED_GPIO_Port, SEM_RED_Pin, GPIO_PIN_RESET);
                 HAL_GPIO_WritePin(SEM_YELLOW_GPIO_Port, SEM_YELLOW_Pin, GPIO_PIN_RESET);
                 HAL_GPIO_WritePin(SEM_GREEN_GPIO_Port, SEM_GREEN_Pin, GPIO_PIN_SET);
@@ -33,6 +39,7 @@ void traffic_light_loop()
                 break;
 
             case PHASE_YELLOW:
+                printf("PHASE: YELLOW\r\n");
                 HAL_GPIO_WritePin(SEM_RED_GPIO_Port, SEM_RED_Pin, GPIO_PIN_RESET);
                 HAL_GPIO_WritePin(SEM_YELLOW_GPIO_Port, SEM_YELLOW_Pin, GPIO_PIN_SET);
                 HAL_GPIO_WritePin(SEM_GREEN_GPIO_Port, SEM_GREEN_Pin, GPIO_PIN_RESET);
